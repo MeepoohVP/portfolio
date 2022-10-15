5d9be55a902df1620c2a649684fcd3a36b33feb1
Pooh <poohvp1147@gmail.com>
2022-10-15 12:43:19 +0700

    responsive 14

--- active.js
+++ active.js
@@ -57,29 +57,29 @@ changeText = () => {
 }
 changeText1 = () => {
     document.getElementById("text").innerHTML = "<span style='color: #ffb6a1;'>ชื่อเล่น</span> พู";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText();rev5();" style="background: #fff;width: 110px;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText2();dot2();">Next >></button>';
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText();rev5();" style="background: #fff;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText2();dot2();">Next >></button>';
 }
 changeText2 = () => {
     document.getElementById("text").innerHTML = "<span style='color: #ffb6a1;'>อายุ</span> 17 ปี";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText1();rev4();" style="background: #fff;width: 110px;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText3();dot3();">Next >></button>'
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText1();rev4();" style="background: #fff;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText3();dot3();">Next >></button>'
 }
 changeText3 = () => {
     document.getElementById("text").innerHTML = "<span style='color: #ffb6a1;'>วันเกิด</span> 11 พฤศจิกายน 2547";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText2();rev3();" style="background: #fff;width: 110px;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText4();dot4();">Next >></button>'
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText2();rev3();" style="background: #fff;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText4();dot4();">Next >></button>'
 }
 changeText4 = () => {
     document.getElementById("text").innerHTML = "<span style='color: #ffb6a1;'>ศึกษาที่</span><br>โรงเรียนบางปะกอกวิทยาคม";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText3();rev2();" style="background: #fff;width: 110px;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText5();dot5()">Next >></button>'
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText3();rev2();" style="background: #fff;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText5();dot5()">Next >></button>'
 }
 
 changeText5 = () => {
     document.getElementById("text").innerHTML = "<span style='color: #ffb6a1;'>งานอดิเรก</span> Coding, เลี้ยงแมว";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText4();rev1();" style="background: #fff;width: 110px;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText6();dot6()">Next >></button>'
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText4();rev1();" style="background: #fff;"><< Previous</button>&nbsp;&nbsp;<button class="next" onclick = "changeText6();dot6()">Next >></button>'
 }
 changeText6 = () => {
-    document.getElementById("text").innerHTML = "<span style='color: #f17572;font-size: 25px;'>My skill</span><br>"
+    document.getElementById("text").innerHTML = "<span class = 'coding'>My skill</span><br>"
         + "<div class = 'icon'><img src = 'html-5.png'>" + "<img src = 'css-3.png'>" + "<img src = 'java-script.png'>"
         + "<img src = 'pyicon.png'>" + "<img src = 'c-logo.png'>" + "<img src = 'git.png'></div>";
-    document.getElementById("next").innerHTML = '<button class="next" onclick ="changeText5();rev6();" style="background: #fff;width: 110px;"><< Previous</button>'
+    document.getElementById("next").innerHTML = '<button class="prev" onclick ="changeText5();rev6();" style="background: #fff;"><< Previous</button>'
 }
 
